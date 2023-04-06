3320b2fc50ad4595c6d4a2eeb4ba18fa847ce2f7
Gabriel Manicucci <gabrielmanicucci@outlook.com>
2023-04-06 18:10:01 -0300

    erros de commit

---
+++ front-vue/src/views/home/HomeComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>{{ legenda }}</h2>
+    <input type="text" :value="legenda">
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['msg'],
+  data: () => {
+    return {
+      legenda: 'Estou no Vue'
+    }
+  }
+
+}
+</script>
+
+<style>
+ h1 {
+  border: 2px solid;
+ }
+
+</style>
