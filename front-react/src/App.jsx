96437f9c010f1e4d3424920c697699b7d228c32c
Gabriel Manicucci <gabrielmanicucci@outlook.com>
2023-04-06 11:10:34 -0300

    delete front-vue

---
+++ front-react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import { Footer } from './views/footer'
+import Home from './views/home/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <Footer></Footer>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
